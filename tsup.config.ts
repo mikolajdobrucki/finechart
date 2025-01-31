d438a77d7b84c0c50e9963edb4621511a92f09dd
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-01-31 10:45:42 +0100

    change bundler to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.tsx"],
+  format: ["cjs", "esm"],
+  dts: true,
+  external: ["react", "react-dom"],
+  clean: true,
+  sourcemap: true,
+});
